22e77784862d0841b01413e3d2a3eddb7a34d5f9
AshkanZj <ashkanzojaji19@gmail.com>
2024-04-27 14:49:01 +0330

    feat: Add Purchase Modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,41 @@
+const Modal = ({ isOpen, toggleOpen }) => {
+  return (
+    <>
+      <section
+        className={`purchase-successful-modal ${isOpen ? "showModal" : ""}`}
+        dir="rtl"
+      >
+        <div className="container animate__animated animate__zoomIn">
+          <div className="psModalContent">
+            <button className="close cursor-pointer" onClick={toggleOpen}>
+              ✖
+            </button>
+            <img src="/assets/images/purchase.png" alt="Winner PNG" />
+            <p>خریدت با موفقیت ثبت شد!</p>
+            <p>از انتخابت پشیمون نمیشی 😉</p>
+            <details className="purchase-details">
+              <summary>
+                سفارش ها
+                <span>
+                  <img
+                    src="./assets/images/Down-Circle-2.svg"
+                    alt="Down Circle 2"
+                    className="purchase-details-img"
+                  />
+                </span>
+              </summary>
+              <section className="foods-details">
+                <section className="foods-details-title">
+                  <p className="food-details-name">نام غذا</p>
+                  <p className="food-details-qty">تعداد</p>
+                </section>
+              </section>
+            </details>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Modal;
